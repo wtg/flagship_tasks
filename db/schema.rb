0d5f31c48dab569f1148bf2c2a17bc2a6a9df32c
August <august@alum.rpi.edu>
2010-08-23 00:56:28 -0400

    Fixed deprecation warning in Rakefile and started storing sessions in the DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100430095157) do
+ActiveRecord::Schema.define(:version => 20100823045209) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
@@ -42,6 +42,16 @@ ActiveRecord::Schema.define(:version => 20100430095157) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "tasks", :force => true do |t|
     t.integer  "project_id"
